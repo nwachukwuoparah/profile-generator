788f3f798823e45b5d75d1000ce071779c14612b
youngmentor <olaleresuliton43@gmail.com>
2023-09-12 17:10:45 +0100

    first commit from suliton

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -30,6 +31,6 @@ function App() {
       </p>
     </>
   )
-}
+};
 
 export default App
