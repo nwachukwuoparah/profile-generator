3faedc93a7dedf7856754fcad3a17ec1de676638
nwachukwuoparah <nwachukwuoparah@gmail.com>
2023-09-13 17:06:04 +0100

    updated profile

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,15 @@
 import { cardType } from "../type.check";
 import "./card.css"
 import React from "react"
-import download from "../../../public/download.jpeg"
-import logoblack from "../../../public/Group 7 black.svg"
+import image from "../../../public/_Downloader 6.png"
+import logoblack from "../../../public/Logo.svg"
+import facebook from "../../../public/facebook.svg"
+import linkdin from "../../../public/linkedin.svg"
+import instagram from "../../../public/instagram.svg"
+import twitter from "../../../public/twitter.svg"
 
 
 const Card: React.FC<cardType> = ({ captureRef }) => {
-
-
 
 
     return (
@@ -16,16 +18,24 @@ const Card: React.FC<cardType> = ({ captureRef }) => {
             className="card"
         >
             <div className="circle"></div>
-            <div className="circle-contain">
+            <div className="card-contain">
                 <img src={logoblack} alt="image" />
-                <div className="card-wrap">
-                    <img src={logoblack} alt="image" />
-                    <h3>Raymond Nkume</h3>
-                    <p>The Curve Africa Cohort 3</p>
-                    <h5>Front-End Trainee</h5>
+                <div className="image-contain">
+                    <img src={image} alt="image" />
                 </div>
+                <h3>Raymond Nkume</h3>
+                <p>The Curve Africa Cohort 3</p>
+                <h5>Front-End Trainee</h5>
             </div>
-
+            <div className="socials">
+                <span>
+                    <img src={facebook} alt="image" />
+                    <img src={linkdin} alt="image" />
+                    <img src={instagram} alt="image" />
+                    <img src={twitter} alt="image" />
+                </span>
+                <p>@ t h e c u r v e a f r i c a</p>
+            </div>
         </div>
     )
 };
